1075a55d07e1a40d3896050c24630af85474c113
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-07-26 16:47:56 -0300

    FEAT: user role

--- src/main/java/com/example/athena/core/entity/enums/UserRole.java
+++ src/main/java/com/example/athena/core/entity/enums/UserRole.java
@@ -1,4 +1,18 @@
 package com.example.athena.core.entity.enums;
 
 public enum UserRole {
-}
+    ADMIN("admin"),
+    MANAGER("manager"),
+    COLLABORATOR("collaborator"),
+    INTERN("intern");
+
+    private final String role;
+
+    UserRole(String role){
+        this.role = role;
+    }
+    public String getRole(){
+        return role;
+    }}
+
+
