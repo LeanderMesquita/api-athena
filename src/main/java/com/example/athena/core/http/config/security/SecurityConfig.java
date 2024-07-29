21a49624de436c2a084126a9ddd191cdec9baacd
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-07-29 17:52:49 -0300

    FIX: syntax conflict in security config

--- src/main/java/com/example/athena/core/http/config/security/SecurityConfig.java
+++ src/main/java/com/example/athena/core/http/config/security/SecurityConfig.java
@@ -8,13 +8,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Autowired
-    SecurityFilter secutiryFilter;
+    SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -28,6 +29,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/auth/profile").permitAll()
                         .anyRequest().authenticated()
                 )
-                .addFilterBefore(secutiryFilter, )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 }
