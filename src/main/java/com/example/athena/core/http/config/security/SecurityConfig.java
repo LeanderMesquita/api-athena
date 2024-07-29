798fe0f98c2ef76d9f941257bea308ea65cea379
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-07-29 17:46:13 -0300

    FEAT: security configurations

--- src/main/java/com/example/athena/core/http/config/security/SecurityConfig.java
+++ src/main/java/com/example/athena/core/http/config/security/SecurityConfig.java
@@ -1,4 +1,33 @@
 package com.example.athena.core.http.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    SecurityFilter secutiryFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/register").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/auth/all-users").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.GET, "/auth/profile").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(secutiryFilter, )
+    }
 }
