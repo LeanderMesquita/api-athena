0155aa25edc408a8bf4859ef71c5a20b47ae7e15
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-07-29 17:47:07 -0300

    FEAT: token service

--- src/main/java/com/example/athena/core/http/service/TokenService.java
+++ src/main/java/com/example/athena/core/http/service/TokenService.java
@@ -1,2 +1,52 @@
-package com.example.athena.core.http.service;public class TokenService {
+package com.example.athena.core.http.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.example.athena.core.entity.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secretKey;
+
+    public String generateToken(User user){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+            return JWT.create()
+                    .withIssuer("api-athena")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(generateExpireDate())
+                    .sign(algorithm);
+
+        }catch(JWTCreationException e){
+            throw new RuntimeException("Error while token generation", e);
+        }
+    }
+
+    public String validateToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
+            return JWT.require(algorithm)
+                    .withIssuer("api-athena")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException e){
+            return "";
+        }
+    }
+
+    private Instant generateExpireDate(){
+        return LocalDateTime.now().plusHours(6).toInstant(ZoneOffset.of("-03:00"));
+    }
 }
