2d6e34978a1d7a637858164e9c0d7f0a9fb95f95
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-07-29 17:47:37 -0300

    FEAT: security chain filter

--- src/main/java/com/example/athena/core/http/config/security/SecurityFilter.java
+++ src/main/java/com/example/athena/core/http/config/security/SecurityFilter.java
@@ -1,2 +1,43 @@
-package com.example.athena.core.http.config.security;public class SecurityFilter {
+package com.example.athena.core.http.config.security;
+
+import com.example.athena.core.http.service.TokenService;
+import com.example.athena.core.repositories.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        if (token != null){
+            var username = tokenService.validateToken(token);
+            UserDetails user = userRepository.findByUsername(username);
+
+            var auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+
 }
