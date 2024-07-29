d945f9f3d9ac6d336b9f3d50cd0a8051cd53cfd8
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-07-29 17:48:26 -0300

    FEAT: authentication controller

--- src/main/java/com/example/athena/core/http/controller/AuthController.java
+++ src/main/java/com/example/athena/core/http/controller/AuthController.java
@@ -1,4 +1,50 @@
 package com.example.athena.core.http.controller;
 
+import com.example.athena.core.entity.User;
+import com.example.athena.core.http.dto.AuthDTO;
+import com.example.athena.core.http.dto.LoginResponseDTO;
+import com.example.athena.core.http.dto.RegisterDTO;
+import com.example.athena.core.http.service.TokenService;
+import com.example.athena.core.repositories.UserRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
 public class AuthController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid AuthDTO data){
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.username(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody @Valid RegisterDTO data){
+        var username = data.name()+" "+data.lastName();
+        if(this.userRepository.findByUsername(username) != null) return ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        User newUser = new User(data.name(), data.lastName(), encryptedPassword, data.role());
+
+        this.userRepository.save(newUser);
+        return ResponseEntity.ok().build();
+    }
+
 }
